895289ec235ff7635aa718c8f668d3641a00aab5
Alejandro Gimenez <alejandrogimenezescuela@gmail.com>
2026-04-17 21:57:34 -0300

    Se agregó un preloader

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+
+    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
     @include('partes.header')
 
     <title>TatamiHUB - @yield('title')</title>
 </head>
 <body>
-
+    <div id="loader">
+    <img src="{{ asset('img/preloader.png') }}" alt="Cargando..." class="pulsate">
+</div>
     <header>
         @include('partes.navbar')
     </header>
@@ -18,5 +22,14 @@
     @include('partes.footer')
 
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script>
+    window.addEventListener('load', function() {
+        const loader = document.getElementById('loader');
+        // Le damos un pequeño delay de medio segundo para que se note el efecto
+        setTimeout(() => {
+            loader.style.display = 'none';
+        }, 500);
+    });
+</script>
 </body>
 </html>

717a2799453349ac064db3af461ef5ea34aefdfd
Lorenzo Marder <marder123lorenzo@gmail.com>
2026-04-22 18:01:39 -0300

    Se modifico el apartado de Inicio de Sesion

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -3,12 +3,6 @@
 @section('content')
 
 <style>
-    input[type="checkbox"] {
-    accent-color: #ff0000; /* El rojo de TatamiHUB */
-    width: 16px;           /* Un poquito más grande para que se vea bien */
-    height: 16px;
-    cursor: pointer;
-}
     /* Contenedor principal para centrar todo en la pantalla */
     .login-wrapper {
         min-height: 85vh; /* Ocupa casi toda la altura de la pantalla */
@@ -175,15 +169,8 @@
                     <button type="submit" class="button-submit">Sign In</button>
                     
                     <p class="p">¿No tenés una cuenta? <span class="span">Crear cuenta</span></p>
-                    <p class="p" style="color: #000000; margin: 15px 0;">O ingresá con</p>
 
                     <div class="flex-row">
-                        <button type="button" class="btn">
-                            <img src="https://www.gstatic.com/images/branding/product/1x/gsa_512dp.png" width="20"> Google
-                        </button>
-                        <button type="button" class="btn">
-                            <svg height="20" width="20" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg> GitHub
-                        </button>
                     </div>
                 </form>
             </div>
